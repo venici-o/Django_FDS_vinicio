f61948eaac9141d6cb5d516dbc3ff1b112ae2f64
Venicio <vinicius290371@gmail.com>
2026-03-18 08:42:06 -0300

    Migração, atualizaçã do SQL, Models e Configurações.

--- project/settings.py
+++ project/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'forum',
+    'forum.apps.ForumConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
